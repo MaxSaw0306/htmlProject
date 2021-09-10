a6be2bd9e7edc31a4449b3fe14ecb5013cd02fd1
MaxSaw0306 <maximusprime563@gmail.com>
2021-09-10 13:02:13 +0200

    df

--- berischtsheft.com/php/mainpage.php
+++ berischtsheft.com/php/mainpage.php
@@ -1,7 +1,7 @@
 <html>
     <head>
-        <link rel="stylesheet" href=".\stylesheets\styles.css"/>
-        <script src=".\scripts\mainscript.js"> </script>
+        <link rel="stylesheet" href="..\stylesheets\styles.css"/>
+        <script src="..\scripts\mainscript.js"> </script>
         <title> Web Berichtsheft </title>
     </head>
     <body>
@@ -12,11 +12,15 @@
             </div>
             <div class="main-page">
                 <div class="side-menu" id="sideMenu">
+                    <ul>
+                        <li>
+                            <p>
+                        </il>
+                    </ul>
                 </div>
                 <div class="content">
                 </div>
             </div>
-            
             <div class="footer">
             </div>
         </div>
