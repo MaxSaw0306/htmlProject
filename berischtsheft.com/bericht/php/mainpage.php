f6148b81a1f978a5999d8dd57671b778ccdafd87
MaxSaw0306 <maximusprime563@gmail.com>
2021-09-10 12:30:57 +0200

    f

---
+++ berischtsheft.com/bericht/php/mainpage.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <link rel="stylesheet" href=".\stylesheets\styles.css"/>
+        <script src=".\scripts\mainscript.js"> </script>
+        <title> Web Berichtsheft </title>
+    </head>
+    <body>
+        <div class="window">
+            <div class="header">
+            </div>
+            <div class="navigator">
+            </div>
+            <div class="main-page">
+                <div class="side-menu" id="sideMenu">
+                </div>
+                <div class="content">
+                </div>
+            </div>
+            
+            <div class="footer">
+            </div>
+        </div>
+    </body>
